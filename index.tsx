f3002145e1fa00d31c3be53bacc3f3af54d77b12
zzw26584 <nguyenhuongrvkns6163@gmail.com>
2026-01-02 10:49:32 +0800

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style.css'; // 引入本地样式
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
